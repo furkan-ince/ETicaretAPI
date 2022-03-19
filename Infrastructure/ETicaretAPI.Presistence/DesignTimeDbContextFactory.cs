c50af4de8d4f367b5ec38748fe61d8b418fbce7d
furka <furka@DESKTOP-PROCYON>
2022-03-20 02:01:13 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Presistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using ETicaretAPI.Presistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Presistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretAPIDbContext>
+    {
+        /// <summary>
+        /// Sadece console ile işlem yapmamız gerektiği durumlarda bu ayarları kullanarak migration işlemleri gerçekleşir.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public ETicaretAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ETicaretAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+
+            return new ETicaretAPIDbContext(dbContextOptionsBuilder.Options);
+        }
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Presistence/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ETicaretAPI.Presistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Presistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPresistenceService(this IServiceCollection services) {
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+        }
+    }
+}
